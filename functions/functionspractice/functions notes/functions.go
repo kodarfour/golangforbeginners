1c8be238fe92d1cbae1e5d1db271837982e7223f
kofid <kodarfour@gmail.com>
2020-06-27 14:57:57 -0400

    finished scopes and functions as values before team meeting

--- functions/functionspractice/functions notes/functions.go
+++ functions/functionspractice/functions notes/functions.go
@@ -2,14 +2,28 @@ package main
 
 import "fmt"
 
+var globe int // global variable
+
 func evenOnly(i int) {
 	if i%2 == 0 {
 		fmt.Printf("%v is even\n", i)
 	} else {
 		return
 	}
+
+	globe = 5 //turns to local variable (within this scope)
+	fmt.Println(globe)
 }
 
 func main() {
 	evenOnly(2)
+
+	globe = 3 //turns to local variable (within this scope)
+
+	fmt.Println(globe)
+	dabMachine := func() { // almost everything in go is a value so u can make functions a variable
+		fmt.Println("*dabs*")
+	}
+	dabMachine()
+
 }

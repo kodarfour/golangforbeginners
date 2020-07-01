e54eea48bdaa7fb6457008f9a4e5ee2c470aaf78
kofid <kodarfour@gmail.com>
2020-07-01 16:45:19 -0400

    map func

--- functions/exercises/mapfunction/mapFunc.go
+++ functions/exercises/mapfunction/mapFunc.go
@@ -2,20 +2,38 @@ package main
 
 import "fmt"
 
-func mapFunc(x func(int) int, y []int) []int{// doubles items in a list
-	for pos := range y{
+func mapFunc(x func(int) int, y []int) []int { // doubles items in a list
+	for pos := range y {
 		y[pos] = x(y[pos])
 	}
 	return y
 }
 
-func main(){
-	doubleIt := func(i int) int{
-		return i *2
+func main() {
+	doubleIt := func(i int) int {
+		return i * 2
 	}
 
-	arr := []int{1,2,3,4}
+	arr := []int{1, 2, 3, 4}
 
-	fmt.Printf("%v ",mapFunc(doubleIt,arr))
+	fmt.Printf("%v ", mapFunc(doubleIt, arr))
+
+}
+
+func mapFunc(x func(int) int, y []int) []int { // doubles items in a list
+	for pos := range y {
+		y[pos] = x(y[pos])
+	}
+	return y
+}
+
+func main() {
+	doubleIt := func(i int) int {
+		return i * 2
+	}
+
+	arr := []int{1, 2, 3, 4}
+
+	fmt.Printf("%v ", mapFunc(doubleIt, arr))
 
 }

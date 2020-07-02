83701a94728581c1a744b4dfc5b49b2f256c08e8
kofid <kodarfour@gmail.com>
2020-07-02 15:09:20 -0400

    fixed mapfunc mistake from last commit

--- functions/exercises/mapfunction/mapFunc.go
+++ functions/exercises/mapfunction/mapFunc.go
@@ -19,21 +19,3 @@ func main() {
 	fmt.Printf("%v ", mapFunc(doubleIt, arr))
 
 }
-
-func mapFunc(x func(int) int, y []int) []int { // doubles items in a list
-	for pos := range y {
-		y[pos] = x(y[pos])
-	}
-	return y
-}
-
-func main() {
-	doubleIt := func(i int) int {
-		return i * 2
-	}
-
-	arr := []int{1, 2, 3, 4}
-
-	fmt.Printf("%v ", mapFunc(doubleIt, arr))
-
-}
